543baf3a1a52e2b9c8a90978f1b600207a83ee1b
Adrian Del Valle <adrianchelo7@gmail.com>
2022-02-23 12:22:09 -0300

    Auth verify

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { showHome, showExplorer, showProfile } = require("../controllers/publicController");
 const publicRouter = express.Router();
-const isAuth = require("../middlewares/isAuth");
+const { isAuthLogged } = require("../middlewares/isAuth");
 
-publicRouter.get("/home", showHome);
+publicRouter.get("/home", isAuthLogged, showHome);
 publicRouter.get("/explorer", showExplorer);
-publicRouter.get("/user", showProfile);
+publicRouter.get("/profile", isAuthLogged, showProfile);
 /* publicRouter.get("/:id", publicController.showProfile); */
 
 module.exports = publicRouter;
