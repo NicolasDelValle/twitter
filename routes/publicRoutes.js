4681abf0e6444aed5433eb72cc273e1638373a0a
FernandoEtcheverry <etchecuenta@gmail.com>
2022-02-24 12:30:02 -0300

    sorry

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,11 +1,17 @@
 const express = require("express");
-const { showHome, showExplorer, showProfile } = require("../controllers/publicController");
+const {
+  showHome,
+  showExplorer,
+  showProfile,
+  showSorry,
+} = require("../controllers/publicController");
 const publicRouter = express.Router();
 const { isAuthLogged } = require("../middlewares/isAuth");
 
 publicRouter.get("/home", isAuthLogged, showHome);
 publicRouter.get("/explorer", showExplorer);
 publicRouter.get("/profile", isAuthLogged, showProfile);
+publicRouter.get("/sorry", showSorry);
 /* publicRouter.get("/:id", publicController.showProfile); */
 
 module.exports = publicRouter;
