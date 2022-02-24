6e02dadbf97c398604ea194f657d36b747bf5340
FernandoEtcheverry <etchecuenta@gmail.com>
2022-02-23 21:03:46 -0300

    validaciones en los models

--- models/User.js
+++ models/User.js
@@ -4,12 +4,35 @@ const bcrypt = require("bcryptjs");
 
 const userSchema = new Schema(
   {
-    firstname: String,
-    lastname: String,
-    username: String,
-    password: String,
+    firstname: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    lastname: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    password: {
+      type: String,
+      unique: true,
+      required: true,
+      minlength: 8,
+      maxlength: 20,
+    },
     email: {
       type: String,
+      unique: true,
       required: true,
       lowercase: true,
     },
@@ -37,29 +60,3 @@ userSchema.methods.validPassword = async function (password) {
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
-
-/* UserSchema.pre('save', function(next) {
-  const user = this;
-
-  // only hash the password if it has been modified (or is new)
-  if (!user.isModified('password')) return next();
-
-  // generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-      if (err) return next(err);
-
-      // hash the password using our new salt
-      bcrypt.hash(user.password, salt, function(err, hash) {
-          if (err) return next(err);
-          // override the cleartext password with the hashed one
-          user.password = hash;
-          next();
-      });
-  });
-});
- */
-/* userSchema.pre("save", async (users, options) => {
-  for (const user of users) {
-    user.password = await bcrypt.hash(user.password, 10);
-  }
-}); */
