50dd486c729599fa3dcda9ecabe102de88763530
Adrian Del Valle <adrianchelo7@gmail.com>
2022-02-25 15:25:48 -0300

    isYourProfile in isAutch.js
    para verificar si es tu perfil :D

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -15,5 +15,14 @@ function isAuthUnlogged(req, res, next) {
     return next();
   }
 }
-
-module.exports = { isAuthLogged, isAuthUnlogged };
+function isYourProfile(req, res, next) {
+  if (req.user._id === req.params.id) {
+    console.log(req.user._id);
+    console.log(req.params.id);
+    console.log("Es tu perfil");
+  } else {
+    console.log("no es tu perfil");
+    return next();
+  }
+}
+module.exports = { isAuthLogged, isAuthUnlogged, isYourProfile };
